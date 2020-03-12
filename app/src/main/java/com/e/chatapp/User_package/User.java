52981e46aa439221a704867d7d622ff8ce682d38
SimingZheng <SimingZheng1999@gmail.com>
2020-03-12 21:39:49 +0000

    Patch

---
+++ app/src/main/java/com/e/chatapp/User_package/User.java
@@ -0,0 +1,47 @@
+package com.e.chatapp.User_package;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String birthday;
+
+    public User(){
+
+    }
+
+    public User(String username,  String email, String birthday,String password){
+        this.username = username;
+        this.email = email;
+        this.birthday = birthday;
+        this.password = password;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday(){
+        return birthday;
+    }
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
