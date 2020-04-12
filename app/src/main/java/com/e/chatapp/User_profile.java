9719b04079b83c23ecd781f76b35eaca04f1ecb2
SimingZheng <SimingZheng1999@gmail.com>
2020-04-12 12:20:16 +0100

    add notes about validation 2.0

--- app/src/main/java/com/e/chatapp/User_profile.java
+++ app/src/main/java/com/e/chatapp/User_profile.java
@@ -3,11 +3,16 @@ package com.e.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,31 +25,44 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class User_profile extends AppCompatActivity {
 
-    private String user_ID;
+    private String user_ID, state, currentuser;
 
-    private DatabaseReference RootRef;
+    private DatabaseReference UserRef, RequestRef, ChatRef;
+    private FirebaseAuth Auth;
 
     private de.hdodenhof.circleimageview.CircleImageView user_portrait;
     private TextView user_name;
     private TextView user_email;
+    private Button btn_request, btn_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        RootRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        RequestRef = FirebaseDatabase.getInstance().getReference().child("Request");
+        ChatRef = FirebaseDatabase.getInstance().getReference().child("Contacts");
+        Auth = FirebaseAuth.getInstance();
+
         user_ID = getIntent().getExtras().get("user_ID").toString();
+        currentuser = Auth.getCurrentUser().getUid();
 
         user_portrait = (CircleImageView) findViewById(R.id.user_portrait);
         user_name = (TextView) findViewById(R.id.user_name);
         user_email = (TextView) findViewById(R.id.user_email);
+        btn_request = (Button) findViewById(R.id.send_requirement);
+        btn_message = (Button) findViewById(R.id.send_message);
+
+        state = "first";
+
+
 
         RetrievePhoto();
     }
         private void RetrievePhoto() {
 
-        RootRef.child(user_ID).addValueEventListener(new ValueEventListener() {
+        UserRef.child(user_ID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("image"))) {
@@ -55,12 +73,16 @@ public class User_profile extends AppCompatActivity {
                     Picasso.get().load(Portrait).placeholder(R.drawable.icon_user).into(user_portrait);
                     user_name.setText(name);
                     user_email.setText(email);
+
+                    RequestManagement();
                 }
                 else {
                     String name = dataSnapshot.child("username").getValue().toString();
                     String email = dataSnapshot.child("email").getValue().toString();
                     user_name.setText(name);
                     user_email.setText(email);
+
+                    RequestManagement();
                 }
             }
             @Override
@@ -68,5 +90,209 @@ public class User_profile extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void RequestManagement(){
+
+        RequestRef.child(currentuser)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(user_ID)){
+                            String request = dataSnapshot.child(user_ID).child("request_type").getValue().toString();
+
+                            if (request.equals("sent")){
+                                state = "sent_request";
+                                btn_request.setText("Cancel Request");
+                            }
+                            else if (request.equals("received")){
+                                state = "receive_request";
+                                btn_request.setText("Accept Request");
+
+                                btn_message.setVisibility(View.VISIBLE);
+                                btn_message.setEnabled(true);
+
+                                btn_message.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        cancelRequest();
+                                    }
+                                });
+                            }
+                        }
+                        else {
+                            ChatRef.child(currentuser)
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.hasChild(user_ID)){
+                                                state = "friend";
+                                                btn_request.setText("Remove friend");
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        if (!currentuser.equals(user_ID)){
+            btn_request.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    btn_request.setEnabled(false);
+
+                    if (state.equals("first")){
+                        sendRequest();
+                    }
+                    if (state.equals("send_request")){
+                        cancelRequest();
+                    }
+                    if (state.equals("receive_request")){
+                        acceptRequest();
+                    }
+                    if (state.equals("friend")){
+                        removeFriend();
+                    }
+                }
+            });
+        }
+        else {
+            btn_request.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void removeFriend(){
+        ChatRef.child(currentuser).child(user_ID)
+                .removeValue()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            ChatRef.child(user_ID).child(currentuser)
+                                    .removeValue()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                btn_request.setEnabled(true);
+                                                state = "first";
+                                                btn_request.setText("Send Request");
+
+                                                btn_message.setVisibility(View.INVISIBLE);
+                                                btn_message.setEnabled(false);
+
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+
+                });
+    }
+
+    private void acceptRequest(){
+        ChatRef.child(currentuser).child(user_ID)
+                .child("Contacts").setValue("Friends")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            ChatRef.child(user_ID).child(currentuser)
+                                    .child("Contacts").setValue("Friends")
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                RequestRef.child(currentuser).child(user_ID)
+                                                        .removeValue()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()){
+                                                                    RequestRef.child(user_ID).child(currentuser)
+                                                                            .removeValue()
+                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                @Override
+                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                    btn_request.setEnabled(true);
+                                                                                    state = "friend";
+                                                                                    btn_request.setText("Remove friend");
+
+                                                                                    btn_message.setVisibility(View.INVISIBLE);
+                                                                                    btn_message.setEnabled(false);
+                                                                                }
+                                                                            });
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+
+    private void cancelRequest(){
+        RequestRef.child(currentuser).child(user_ID)
+                .removeValue()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            RequestRef.child(user_ID).child(currentuser)
+                                    .removeValue()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                btn_request.setEnabled(true);
+                                                state = "first";
+                                                btn_request.setText("Send Request");
+
+                                                btn_message.setVisibility(View.INVISIBLE);
+                                                btn_message.setEnabled(false);
+
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+
+                });
+    }
+
+    private void sendRequest(){
+        RequestRef.child(currentuser).child(user_ID)
+                .child("request_type").setValue("sent")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            RequestRef.child(user_ID).child(currentuser)
+                                    .child("request_type").setValue("received")
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                btn_request.setEnabled(true);
+                                                state = "send_request";
+                                                btn_request.setText("Cancel Request");
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/e/chatapp/ui/friendlist/FriendlistFragment.java
+++ app/src/main/java/com/e/chatapp/ui/friendlist/FriendlistFragment.java
@@ -1,9 +1,13 @@
 package com.e.chatapp.ui.friendlist;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,19 +17,36 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-//import com.e.chatapp.Friend_list_Adapter;
+import com.e.chatapp.Nearby_user;
 import com.e.chatapp.R;
 import com.e.chatapp.User_package.Friendlist_item;
-
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class FriendlistFragment extends Fragment {
 
+    private View root;
     private FriendlistViewModel friendlistViewModel;
     private List<Friendlist_item> friendList = new ArrayList<>();
+    private RecyclerView Friend_Recycle;
+
+    private DatabaseReference FriendRef, UsersRef;
+    private FirebaseAuth Auth;
+    private String currentuser;
+
+    public FriendlistFragment(){
+
+    }
 
     public View onCreateView(@Nullable LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -33,37 +54,84 @@ public class FriendlistFragment extends Fragment {
                 ViewModelProviders.of(this).get(FriendlistViewModel.class);
         assert inflater != null;
         View root = inflater.inflate(R.layout.fragment_friends_list, container, false);
-//
-//        super.onCreate(savedInstanceState);
-//
-//        initItems();
-//        RecyclerView recyclerView =(RecyclerView) root.findViewById(R.id.recycler_view);
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
-//        recyclerView.setLayoutManager(layoutManager);
-//        Friend_list_Adapter adapter = new Friend_list_Adapter(friendList);
-//        recyclerView.setAdapter(adapter);
 
-//        SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
-//            @Override
-//            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
-//                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);
-//                return new ClassicsHeader(context);
-//            }
-//        });
+        Friend_Recycle = (RecyclerView) root.findViewById(R.id.friend_recycler_view);
+        Friend_Recycle.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        Auth = FirebaseAuth.getInstance();
+        currentuser = Auth.getCurrentUser().getUid();
+        FriendRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentuser);
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         return root;
-}
-}
+    }
 
-//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-//
-//    }
-//
-//    private void initItems() {
-//        for (int i = 0; i < 10; i++) {
-//            Friendlist_item portrait = new Friendlist_item("User1", R.drawable.icon_user);
-//            friendList.add(portrait);
-//            Friendlist_item portrait2 = new Friendlist_item("User2", R.drawable.icon_user);
-//            friendList.add(portrait2);
-//        }
-//    }
-//}
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        FirebaseRecyclerOptions options =
+                new FirebaseRecyclerOptions.Builder<Friendlist_item>()
+                .setQuery(FriendRef, Friendlist_item.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Friendlist_item, FriendListViewHolder> adapter
+                = new FirebaseRecyclerAdapter<Friendlist_item, FriendListViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final FriendListViewHolder friendListViewHolder, int i, @NonNull Friendlist_item friendlist_item) {
+                String userID = getRef(i).getKey();
+
+                UsersRef.child(userID).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild("image")){
+                            String profileImage = dataSnapshot.child("image").getValue().toString();
+                            String profileemail = dataSnapshot.child("email").getValue().toString();
+                            String profilename = dataSnapshot.child("username").getValue().toString();
+
+                            friendListViewHolder.username.setText(profilename);
+                            friendListViewHolder.email.setText(profileemail);
+                            Picasso.get().load(profileImage).placeholder(R.drawable.icon_user).into(friendListViewHolder.image);
+                        }
+                        else {
+                            String profileemail = dataSnapshot.child("email").getValue().toString();
+                            String profilename = dataSnapshot.child("username").getValue().toString();
+
+                            friendListViewHolder.username.setText(profilename);
+                            friendListViewHolder.email.setText(profileemail);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public FriendListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.friend_list_item, parent, false);
+                FriendListViewHolder viewHolder = new FriendListViewHolder(view);
+                return viewHolder;
+            }
+        };
+
+        Friend_Recycle.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class FriendListViewHolder extends RecyclerView.ViewHolder{
+        TextView username, email;
+        ImageView image;
+
+        public FriendListViewHolder (@NonNull View itemView){
+            super(itemView);
+
+            username = itemView.findViewById(R.id.friend_name);
+            email = itemView.findViewById(R.id.friend_email);
+            image = itemView.findViewById(R.id.friend_portrait);
+        }
+    }
+}
